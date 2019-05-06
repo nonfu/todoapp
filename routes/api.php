77c1ee221fc01df55805dcbec4b6d35d00f359e0
qiang.sun <qiang.sun@husor.com>
2019-05-06 12:31:25 +0800

    Dingo API

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,32 @@ Route::middleware('auth:api')->group(function (){
     });
     Route::resource('tasks', 'TaskController', ['except' => ['create', 'edit']]);
 });
+
+// Dingo API Endpoint
+$api = app(\Dingo\Api\Routing\Router::class);
+
+$api->version('v1', function ($api) {
+    $api->get('/task/{id}', function ($id) {
+        return \App\Task::findOrFail($id);
+    })->name('task.detail');
+    $api->get('/task/{id}/{url}', function ($id) {
+        $url = app(\Dingo\Api\Routing\UrlGenerator::class)
+            ->version('v1')
+            ->route('task.detail', ['id' => $id]);
+        return $url;
+    });
+});
+
+$api->version('v2', function ($api) {
+    $api->get('/task/{id}', function ($id) {
+        return \App\Task::findOrFail($id);
+    });
+    $api->get('/task/{id}/{url}', function ($id) {
+        $url = app(\Dingo\Api\Routing\UrlGenerator::class)
+            ->version('v2')
+            ->route('task.detail', ['id' => $id]);
+        return $url;
+    });
+});
+
+
